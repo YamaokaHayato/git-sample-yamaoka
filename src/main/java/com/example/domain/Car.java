a6ecc824dacc8bf5bc892886a51d3db390a3ad05
YamaokaHayato <hayatoyamaoka0804@gmail.com>
2022-11-01 11:07:03 +0900

    Carクラスのgetter,setterの追加

--- src/main/java/com/example/domain/Car.java
+++ src/main/java/com/example/domain/Car.java
@@ -5,4 +5,22 @@ public class Car {
 	private String name;
 	private Integer speed;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+	
+	
+
 }
